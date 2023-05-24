b888453207356654440de1b8aa7ab750d995c1b8
pie4fools <64813630+pie4fools@users.noreply.github.com>
2023-05-23 21:08:13 -0500

    Added product images

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
+import productimg1 from '../assets/productimg1.jpg'
+import productimg2 from '../assets/productimg2.jpg'
+import productimg3 from '../assets/productimg3.jpg'
+import productimg4 from '../assets/productimg4.jpg'
+
 
 const Products = () => {
     return (
         <>
-        <div className='about-image'>
-          {/* Background image for about page */}
-        <div className='productscontainer'>
-        <div className='productsbox2'></div>
-        <div className='productsbox3'></div>
-        <div className='productsbox4'></div>
-        <div className='productsbox5'></div>
-        <div className='productsbox6'></div>
-        <div className='productsbox7'></div>
-        </div>
+        {/* Background image for about page */}
+        <div className='about-image pt-10'>
+        {/* Current deals box */}
+        <div class='productBox'><img className='mx-auto scale-50' src={productimg1}></img></div>
+        <div class='productBox'><img className='mx-auto scale-50' src={productimg2}></img></div> 
+        <div class='productBox'><img className='mx-auto scale-50' src={productimg3}></img></div> 
+        <div class='productBox'><img className='mx-auto scale-50' src={productimg4}></img></div>       
         <div>
             <p className='pt-20 pb-2 mx-auto text-center font-semibold relative font-Jost'/>
         </div>
             {/* CTA button for products */}
             <div className='flex justify-end'>
-                <btn className='pt-6 flex relative w-[215px] rounded-full h-[73px] bg-[#1E9500] text-white text-center text-xl mx-auto justify-center font-Jost'>About</btn>
+                <btn className='pt-6 mb-5 flex relative w-[215px] rounded-full h-[73px] bg-[#1E9500] text-white text-center text-xl mx-auto justify-center font-Jost'>About</btn>
             </div>
         </div>
         </>
